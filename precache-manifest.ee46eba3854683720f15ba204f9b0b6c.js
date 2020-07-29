82d4e4c90a26364dfabdc90152d4ac3f5fa2e64e
dipak12399 <68970316+dipak12399@users.noreply.github.com>
2020-07-29 15:12:10 -0500

    initial commit

---
+++ precache-manifest.ee46eba3854683720f15ba204f9b0b6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af80434c82c11b43370d9e8874b3512c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cf464503082a0d31dca",
+    "url": "/static/css/2.0d0e89dc.chunk.css"
+  },
+  {
+    "revision": "e1dd391c23bdc79dd45f",
+    "url": "/static/css/main.c17b4d44.chunk.css"
+  },
+  {
+    "revision": "1cf464503082a0d31dca",
+    "url": "/static/js/2.83493657.chunk.js"
+  },
+  {
+    "revision": "e7c69aa4549a0938e155d41214f1973f",
+    "url": "/static/js/2.83493657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1dd391c23bdc79dd45f",
+    "url": "/static/js/main.ec52b5b9.chunk.js"
+  },
+  {
+    "revision": "28c89a97fd919bc448d0",
+    "url": "/static/js/runtime-main.40ff8a99.js"
+  }
+]);
